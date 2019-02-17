50c6d8c7d45b62d0c29cf5ee219b0471bd7fe896
Vinoth <vinu.barro@gmail.com>
2019-02-16 19:43:51 -0500

    subset remove duplicates

--- recursion/generate_subset.py
+++ recursion/generate_subset.py
@@ -5,10 +5,13 @@ def generate_all_subsets(s):
     def _generate_all_subsets(so_far, idx):
 
         if (idx == len(s)):
-            output.append("".join(so_far))
-            return
+           output.append("".join(so_far))
+           return
 
         _generate_all_subsets(so_far+[s[idx]], idx+1)
+        ##remove duplicates
+        while idx+1 < len(s) and s[idx+1] == s[idx]:
+            idx+=1
         _generate_all_subsets(so_far,idx+1)
 
 
@@ -18,6 +21,7 @@ def generate_all_subsets(s):
 
     return output
 
-s = ['x','y']
+#s = ['x','y']
+s = ['1','2', '3','1']
 res = generate_all_subsets(s)
 print(res)
